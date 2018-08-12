e7058633bedef6628b387540a53697e80dee3563
lazaryan <lazaryan99@gmail.com>
2018-08-12 13:20:18 +0300

    finish about us

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,1 +1,16 @@
 ;"use strict";
+
+let galerry             = document.querySelector('.js-grid');
+let project             = document.querySelector('#popupProject');
+let closeProject        = document.querySelector('#closePopupProject');
+
+galerry.addEventListener('click', (e) => {
+        let target = e.target;
+        if(target.classList.contains('js-grid__image')){
+                showPopupGallery();
+        }
+}, true);
+
+function showPopupGallery () {
+        closeProject.classList.remove('_none');
+}
